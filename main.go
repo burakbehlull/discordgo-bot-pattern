ed056152bb217c15c6f63936bcf6cfb7bf2f383a
burakbehlull <burakyabgu@gmail.com>
2024-10-23 14:36:04 +0300

    chore: Create the bot's connection infrastructure

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	discord "github.com/bwmarrin/discordgo"
+)
+
+var (
+	client *discord.Session
 )
 
 func main() {
-	fmt.Println("Merhaba discordgo bot!")
+	flag.Parse()
+
+	c, err := discord.New("Bot " + "")
+	if err != nil {
+		fmt.Println("Hata: ", err)
+	}
+
+	client = c
+
+	err = client.Open()
+	if err != nil {
+		fmt.Println("Bot giriş yapmadı!")
+	}
+
+	fmt.Println("Bot giriş yaptı!")
+	shutdownChannel()
+}
+
+func shutdownChannel() {
+	sc := make(chan os.Signal, 1)
+	signal.Notify(
+		sc,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		os.Interrupt,
+	)
+
+	<-sc
 }
